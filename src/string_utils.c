0cdd64263c5f45e48853a2a1f247af9d395a7595
ELaamraniG <moha.mati@hotmail.com>
2025-04-15 18:27:09 +0100

    continue parser skeleton

--- src/string_utils.c
+++ src/string_utils.c
@@ -1,16 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   string_utils.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jhamdaou <jhamdaou@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 14:51:39 by moel-amr          #+#    #+#             */
-/*   Updated: 2025/04/15 12:09:58 by jhamdaou         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../includes/minishell.h"
+
+int	ft_isspace(int c)
+{
+	if (c == ' ' || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -27,4 +22,49 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	ft_strlcpy(ss, s + start, len + 1);
 	return (ss);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	slen;
+
+	if (!dst || !src)
+		return (0);
+	i = 0;
+	slen = ft_strlen(src);
+	if (dstsize == 0)
+		return (slen);
+	while (i < dstsize - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	return (slen);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*snew;
+	int		slen;
+
+	slen = ft_strlen(s1);
+	snew = malloc(sizeof(char) * (slen + 1));
+	if (snew == NULL)
+		return (NULL);
+	ft_strlcpy(snew, s1, slen + 1);
+	return (snew);
 }
