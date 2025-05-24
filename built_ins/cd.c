8c7c6c78d9f9221799a46dcdfbdebede22bcc3e1
Bladesjj <hamdaoui704@gmail.com>
2025-05-24 14:44:02 +0100

    update cd

--- built_ins/cd.c
+++ built_ins/cd.c
@@ -1,24 +1,33 @@
 #include "../includes/minishell.h"
 
-int	change_dir(char **args, t_env_list *env)
+int    change_dir(char **args, t_env_list *env)
 {
-	int			val;
-	t_env_list *home;
+    int            val;
+    char        *old;
+    t_env_list    *home;
+    t_env_list    *pwd;
+    t_env_list    *old_pwd;
 
-	home = get_env_value(env, "HOME");
-	if (!args ||  !args[0] || ft_strcmp(args[0], "cd") != 0)
-		return 1;
-	val = chdir(args[1]);
-	if (!args[1] && get_env_value(env, "HOME") == NULL) // for if we unset HOME
-	{
-		ft_putstr_fd(2, "minishell: cd: HOME not set\n");
-		return (1);
-	}
-	else if (!args[1]) // redirecting to home
-		chdir(home->value);
-	if (val == -1 && args[1]) // if invalid direction
-	{
-		perror(args[1]);
-	}
-	return 0;
+    home = get_env_value(env, "HOME");
+    pwd = get_env_value(env, "PWD");
+    old_pwd = get_env_value(env, "OLDPWD");
+    old = getcwd(NULL, 0);
+    if (!args ||  !args[0] || ft_strcmp(args[0], "cd") != 0)
+        return 1;
+    val = chdir(args[1]);
+    if (!args[1] && get_env_value(env, "HOME") == NULL) // for if we unset HOME
+    {
+        ft_putstr_fd(2, "minishell: cd: HOME not set\n");
+        return (1);
+    }
+    else if (!args[1]) // redirecting to home
+        chdir(home->value);
+    if (val == -1 && args[1]) // if invalid direction
+    {
+        perror(args[1]);
+        return (1);
+    }
+    old_pwd->value = old;
+    insert_node_last(&env, pwd->key, getcwd(NULL, 0), 0);
+    return 0;
 }
