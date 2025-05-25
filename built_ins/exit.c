df02ca05a2b6e9aad70e38355fc9a961ff3719bf
Bladesjj <hamdaoui704@gmail.com>
2025-05-25 12:19:50 +0100

    update exit

--- built_ins/exit.c
+++ built_ins/exit.c
@@ -15,11 +15,8 @@ static int is_allnum(char *s)
 
 }
 
-int	exit_shell(char **args)
+int	exit_shell(char **args, int status)
 {
-	int	exit_code;
-
-	exit_code = 5222;
 	if (args[1])
 	{
 		if (args[2])
@@ -27,18 +24,17 @@ int	exit_shell(char **args)
 			ft_putstr_fd(2, "minishell: exit: too many arguments\n");
 			return (1);
 		}
-		if (!s_allnum(args[1]))
+		if (!is_allnum(args[1]))
 		{
 			ft_putstr_fd(2, "bash: exit: ");
 			ft_putstr_fd(2, args[1]);
 			ft_putstr_fd(2, ": numeric argument required\n");
 			return (1);
 		}
-		exit(155);
+		ft_putstr_fd(1, "exit\n");
+		exit(args[1]);
 	}
 	ft_putstr_fd(1, "exit\n");
-	if (!args[1])
-		exit(0); 
-	exit(exit_code);
-	return (exit_code);
+	exit(status); 
+	return (status);
 }
