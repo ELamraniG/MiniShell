53d9fcf50d12144ccb8a781e47e6aa75b7924071
ELamraniG <moha.mati@hotmail.com>
2025-05-26 17:35:15 +0100

    more norminette fixes

--- execution/gluing_stuff.c
+++ execution/gluing_stuff.c
@@ -1,11 +1,37 @@
 #include "../includes/minishell.h"
 
+static void	norm_join_args(int *i, int *j, char ***args2, int count)
+{
+	*args2 = malloc(sizeof(char *) * (count + 1));
+	*i = 1;
+	*j = 0;
+}
+
+void norm_args2(t_ast_tree *astree,int *i,int *j,char ***args2)
+{
+	char *tmp;
+		while (*i < astree->arg_counter)
+	{
+		if (astree->is_space[*i - 1])
+			args2[0][++(*j)] = ft_strdup(astree->args[*i]);
+		else
+		{
+			tmp = args2[0][*j];
+			args2[0][*j] = ft_strjoin(tmp, astree->args[*i]);
+			free(tmp);
+		}
+		(*i)++;
+	}
+}
+
 char	**join_args_without_spaces(t_ast_tree *astree)
 {
 	char	**args2;
 	char	*tmp;
+	int		i;
+	int		j;
+	int		count;
 
-	int i, j, count;
 	count = 1;
 	i = 0;
 	while (i < astree->arg_counter - 1)
@@ -16,35 +42,14 @@ char	**join_args_without_spaces(t_ast_tree *astree)
 	}
 	if (count == astree->arg_counter)
 		return (astree->args);
-	args2 = malloc(sizeof(char *) * (count + 1));
-	j = 0;
+	norm_join_args(&i, &j, &args2, count);
 	args2[0] = ft_strdup(astree->args[0]);
-	i = 1;
-	while (i < astree->arg_counter)
-	{
-		if (astree->is_space[i - 1])
-		{
-			j++;
-			args2[j] = ft_strdup(astree->args[i]);
-		}
-		else
-		{
-			tmp = args2[j];
-			args2[j] = ft_strjoin(tmp, astree->args[i]);
-			free(tmp);
-		}
-		i++;
-	}
+	norm_args2(astree,&i,&j,&args2);
 	args2[count] = NULL;
 	i = 0;
 	while (i < astree->arg_counter)
-	{
-		free(astree->args[i]);
-		i++;
-	}
-	free(astree->args);
-	astree->arg_counter = count;
-	return (args2);
+		free(astree->args[i++]);
+	return (free(astree->args), astree->arg_counter = count, args2);
 }
 
 static char	*join_files_without_spaces(t_redirect *redir)
@@ -66,7 +71,7 @@ static char	*join_files_without_spaces(t_redirect *redir)
 }
 void	join_all_redir_files_without_spaces(t_ast_tree *astree)
 {
-	t_redirect	*tmp;
+	t_redirect *tmp;
 
 	tmp = astree->redirect;
 	while (tmp)
