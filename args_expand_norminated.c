c9427b46e93d9911e89c25fa80273a962fe3fe9c
ELamraniG <moha.mati@hotmail.com>
2025-05-27 12:17:38 +0100

    refactor into more structs

--- args_expand_norminated.c
+++ args_expand_norminated.c
@@ -1,5 +1,17 @@
 #include "includes/minishell.h"
 
+typedef struct s_expd2 {
+	int		i;
+	int		j;
+	int		flag;
+	int		prev_pos;
+	char	**dble;
+	char	*tmp;
+	char	*tmp2;
+	char	*tmp3;
+	t_env_list *t;
+} t_expd2;
+
 void	ft_realloc(t_expd_norm *expd, char *s)
 {
 	int		i;
@@ -46,33 +58,31 @@ static void	trim_the_args(t_expd_norm *expd)
 	return ;
 }
 
-static char	*get_keyy(char *str, t_env_list *env, int prev_pos, int *i,
+static char	*get_keyy(char *str, t_env_list *env, t_expd2 *expd2,
 		int status)
 {
-	char	*tmp;
 	int		len;
 
-	(*i)++;
-	tmp = NULL;
-	// i skip the $
+	(expd2->i)++;
+	expd2->tmp = NULL;
 	len = 0;
-	if (str[*i] == '?')
+	if (str[expd2->i] == '?')
 	{
-		(*i)++;
+		(expd2->i)++;
 		return (ft_strdup("?"));
 	}
-	if (!ft_isalpha(str[*i]))
+	if (!ft_isalpha(str[expd2->i]))
 		return (ft_strdup("$"));
-	while (str[*i])
+	while (str[expd2->i])
 	{
-		if (str[*i] == '$' || (!ft_isalnum(str[*i]) && str[*i] != '_'))
-			return (ft_substr(str, prev_pos + 1, *i - prev_pos - 1));
+		if (str[expd2->i] == '$' || (!ft_isalnum(str[expd2->i]) && str[expd2->i] != '_'))
+			return (ft_substr(str, expd2->prev_pos + 1, expd2->i - expd2->prev_pos - 1));
 		len++;
-		(*i)++;
+		(expd2->i)++;
 	}
 	if (len == 0)
 		return (ft_strdup("$"));
-	return (ft_strdup(str + prev_pos + 1));
+	return (ft_strdup(str + expd2->prev_pos + 1));
 }
 
 static int	has_space_at_the_end(char *s)
@@ -106,18 +116,9 @@ static int	has_space_at_the_beginning(char *s)
 static void	expanded_for_single_word(t_expd_norm *expd, t_env_list *env,
 		int status, t_ast_tree *astree)
 {
-	int		i;
-	int		j;
-	int		flag;
-	int		prev_pos;
-	char	**dble;
-	char	*tmp3;
-	char	*tmp;
-	char	*tmp2;
-				t_env_list *t;
+	t_expd2 expd2;
 
-	i = 0;
-	j = 0;
+	expd2.i = 0;
 	if (astree->q_type[expd->k] == SQ)
 	{
 		ft_realloc(expd, ft_strdup(astree->args[expd->k]));
@@ -125,71 +126,71 @@ static void	expanded_for_single_word(t_expd_norm *expd, t_env_list *env,
 		expd->q_type[expd->size - 1] = astree->q_type[expd->k];
 		return ;
 	}
-	flag = 0;
-	prev_pos = 0;
-	while (astree->args[expd->k][i])
+	expd2.flag = 0;
+	expd2.prev_pos = 0;
+	while (astree->args[expd->k][expd2.i])
 	{
-		j = 0;
-		if (astree->args[expd->k][i] == '\'')
-			i++;
-		else if (astree->args[expd->k][i] == '$')
+		expd2.j = 0;
+		if (astree->args[expd->k][expd2.i] == '\'')
+			expd2.i++;
+		else if (astree->args[expd->k][expd2.i] == '$')
 		{
-			flag = 1;
-			tmp = ft_substr(astree->args[expd->k], prev_pos, i - prev_pos);
-			prev_pos = i;
-			if (tmp[0] != 0)
+			expd2.flag = 1;
+			expd2.tmp = ft_substr(astree->args[expd->k], expd2.prev_pos, expd2.i - expd2.prev_pos);
+			expd2.prev_pos = expd2.i;
+			if (expd2.tmp[0] != 0)
 			{
-				ft_realloc(expd, tmp);
+				ft_realloc(expd, expd2.tmp);
 				expd->is_space[expd->size - 1] = 0;
 				expd->q_type[expd->size - 1] = astree->q_type[expd->k];
 			}
 			else
-				free(tmp);
-			tmp2 = get_keyy(astree->args[expd->k], env, prev_pos, &i, status);
-			if (astree->args[expd->k][i])
-				flag = 0;
-			if (!ft_strcmp(tmp2, "?"))
+				free(expd2.tmp);
+			expd2.tmp2 = get_keyy(astree->args[expd->k], env, &expd2, status);
+			if (astree->args[expd->k][expd2.i])
+				expd2.flag = 0;
+			if (!ft_strcmp(expd2.tmp2, "?"))
 			{
-				tmp3 = ft_itoa(status);
-				flag = 0;
+				expd2.tmp3 = ft_itoa(status);
+				expd2.flag = 0;
 			}
-			else if (!ft_strcmp(tmp2, "$"))
+			else if (!ft_strcmp(expd2.tmp2, "$"))
 			{
-				tmp3 = ft_strdup("$");
-				flag = 0;
+				expd2.tmp3 = ft_strdup("$");
+				expd2.flag = 0;
 			}
 			else
 			{
-				t = get_env_value(env, tmp2);
-				if (!t)
+				expd2.t = get_env_value(env, expd2.tmp2);
+				if (!expd2.t)
 				{
-					tmp3 = ft_strdup("");
-					ft_realloc(expd, tmp3);
-					free(tmp2);
+					expd2.tmp3 = ft_strdup("");
+					ft_realloc(expd, expd2.tmp3);
+					free(expd2.tmp2);
 					expd->is_space[expd->size - 1] = 0;
 					expd->q_type[expd->size - 1] = DQ;
-					prev_pos = i;
+					expd2.prev_pos = expd2.i;
 					continue ;
 				}
 				else
-					tmp3 = ft_strdup(t->value);
+					expd2.tmp3 = ft_strdup(expd2.t->value);
 			}
-			free(tmp2);
-			dble = ft_split_for_expand(tmp3, ' ');
-			free(tmp3);
-			while (dble[j])
+			free(expd2.tmp2);
+			expd2.dble = ft_split_for_expand(expd2.tmp3, ' ');
+			free(expd2.tmp3);
+			while (expd2.dble[expd2.j])
 			{
-				ft_realloc(expd, dble[j]);
+				ft_realloc(expd, expd2.dble[expd2.j]);
 				expd->is_space[expd->size - 1] = 1;
 				expd->q_type[expd->size - 1] = NQ;
-				if (j == 0)
+				if (expd2.j == 0)
 				{
 					if (has_space_at_the_beginning(expd->args[expd->size - 1])
 						&& expd->size > 1)
 						expd->is_space[expd->size - 2] = 1;
 				}
-				j++;
-				if (dble[j] == NULL)
+				expd2.j++;
+				if (expd2.dble[expd2.j] == NULL)
 				{
 					if (has_space_at_the_end(expd->args[expd->size - 1]))
 						expd->is_space[expd->size - 1] = 1;
@@ -198,16 +199,16 @@ static void	expanded_for_single_word(t_expd_norm *expd, t_env_list *env,
 				}
 				trim_the_args(expd);
 			}
-			free(dble);
-			prev_pos = i;
+			free(expd2.dble);
+			expd2.prev_pos = expd2.i;
 		}
 		else
-			i++;
+			expd2.i++;
 	}
-	if (flag == 0)
+	if (expd2.flag == 0)
 	{
-		tmp = ft_substr(astree->args[expd->k], prev_pos, ft_strlen(astree->args[expd->k]) - prev_pos);
-		ft_realloc(expd, tmp);
+		expd2.tmp = ft_substr(astree->args[expd->k], expd2.prev_pos, ft_strlen(astree->args[expd->k]) - expd2.prev_pos);
+		ft_realloc(expd, expd2.tmp);
 		expd->is_space[expd->size - 1] = astree->is_space[expd->k];
 		expd->q_type[expd->size - 1] = astree->q_type[expd->k];
 	}
